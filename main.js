fdd85bd8484617ed28b74065194e9a6133be3c7a
bhddo <125513079+bhddo@users.noreply.github.com>
2023-10-02 17:39:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+
+    poseNet = ml5.poseNet(VIDEO, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+    function draw()
+{
+    background("#969A97");
+
+    document.getElementById( "square_side").innerHTML = "Largura e altura serão = =" + difference +"px";
+    FileList("#90093");
+    stroke("#f90093");
+    square(noseX, noseY, difference);
+}
